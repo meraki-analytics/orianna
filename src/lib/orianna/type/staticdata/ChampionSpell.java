e38b0f3bc39bade9be4b4af4adfca6bc9d31352e
Rob Rua <robertrua@gmail.com>
2014-08-31 23:50:01 -0400

    Added methods to replace tooltip variables with numerical values in ChampionSpell and SummonerSpell

--- src/lib/orianna/type/staticdata/ChampionSpell.java
+++ src/lib/orianna/type/staticdata/ChampionSpell.java
@@ -66,12 +66,70 @@ public class ChampionSpell implements Serializable {
         return true;
     }
 
+    /**
+     * Replaces the variables in the sanitized tooltip with numerical values
+     *
+     * @param championLevel
+     *            the champion level to get the specific sanititzed tooltip for
+     * @param spellLevel
+     *            the spell rank to get the specific sanitized tooltip for
+     * @return the sanitized tooltip with numerical values
+     */
+    public String getSanitizedTooltip(final int champLevel, final int spellLevel) {
+        return replaceVariables(sanitizedTooltip, champLevel, spellLevel);
+    }
+
+    /**
+     * Replaces the variables in the tooltip with numerical values
+     *
+     * @param championLevel
+     *            the champion level to get the specific tooltip for
+     * @param spellLevel
+     *            the spell rank to get the specific tooltip for
+     * @return the tooltip with numerical values
+     */
+    public String getTooltip(final int champLevel, final int spellLevel) {
+        return replaceVariables(tooltip, champLevel, spellLevel);
+    }
+
     @Override
     public int hashCode() {
         final int prime = 31;
         int result = 1;
         result = prime * result + (key == null ? 0 : key.hashCode());
         return result;
+    }
+
+    private String replaceVariables(String text, final int champLevel, final int spellLevel) {
+        if(spellLevel < 1 || spellLevel > maxRank) {
+            throw new IllegalArgumentException("Not a valid level for this spell!");
+        }
+        if(champLevel < 1 || champLevel > 18) {
+            throw new IllegalArgumentException("Not a valid champion level!");
+        }
+
+        if(effect != null) {
+            int i = 1;
+            for(final List<Double> e : effect) {
+                text = text.replaceAll("\\{\\{ e" + i + " \\}\\}", e.get(spellLevel - 1).toString());
+                i++;
+            }
+        }
+
+        if(vars != null) {
+            for(final SpellVariables var : vars) {
+                Double val = var.coeff.get(0);
+                if(var.coeff.size() == maxRank) {
+                    val = var.coeff.get(spellLevel - 1);
+                }
+                else if(var.coeff.size() == 18) {
+                    val = var.coeff.get(champLevel - 1);
+                }
+                text = text.replaceAll("\\{\\{ " + var.key + " \\}\\}", val.toString());
+            }
+        }
+
+        return text;
     }
 
     @Override
--- src/lib/orianna/type/staticdata/SummonerSpell.java
+++ src/lib/orianna/type/staticdata/SummonerSpell.java
@@ -68,12 +68,49 @@ public class SummonerSpell implements Serializable {
         return true;
     }
 
+    /**
+     * Replaces the variables in the sanitized tooltip with numerical values
+     *
+     * @param championLevel
+     *            the champion level to get the specific sanitized tooltip for
+     * @return the sanitized tooltip with numerical values
+     */
+    public String getSanitizedTooltip(final int championLevel) {
+        return replaceVariables(sanitizedTooltip, championLevel);
+    }
+
+    /**
+     * Replaces the variables in the tooltip with numerical values
+     *
+     * @param championLevel
+     *            the champion level to get the specific tooltip for
+     * @return the tooltip with numerical values
+     */
+    public String getTooltip(final int championLevel) {
+        return replaceVariables(tooltip, championLevel);
+    }
+
     @Override
     public int hashCode() {
         final int prime = 31;
         int result = 1;
         result = prime * result + (ID == null ? 0 : ID.hashCode());
         return result;
+    }
+
+    private String replaceVariables(String text, final int level) {
+        if(level < 1 || level > 18) {
+            throw new IllegalArgumentException("Not a valid champion level!");
+        }
+
+        if(vars != null) {
+            for(final SpellVariables var : vars) {
+                final Double val = var.link.equals("@player.level") ? var.coeff.get(level - 1) : var.coeff.get(0);
+                text = text.replaceAll("\\{\\{ " + var.key + " \\}\\}", val.toString());
+            }
+        }
+
+        return text;
     }
 
     @Override
