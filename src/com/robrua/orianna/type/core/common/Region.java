35c4af16c51a2e6c0f3bb15ed6a927a716911327
Rob Rua <robertrua@gmail.com>
2016-04-05 17:21:31 -0400

    Add japan region

--- src/com/robrua/orianna/type/core/common/Region.java
+++ src/com/robrua/orianna/type/core/common/Region.java
@@ -4,7 +4,7 @@ import com.robrua.orianna.api.core.RiotAPI;
 import com.robrua.orianna.type.core.status.ShardStatus;
 
 public enum Region {
-    BR, EUNE, EUW, KR, LAN, LAS, NA, OCE, PBE, RU, TR;
+    BR, EUNE, EUW, JP, KR, LAN, LAS, NA, OCE, PBE, RU, TR;
 
     /**
      * The status shard for this region
