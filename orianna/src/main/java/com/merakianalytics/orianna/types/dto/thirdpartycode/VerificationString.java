a19bd8446fe9ebd86d795f64dfcc400a5cd34604
Rob Rua <robertrua@gmail.com>
2017-12-16 03:20:23 -0500

    serialization for VericationStrings

--- orianna/src/main/java/com/merakianalytics/orianna/types/dto/thirdpartycode/VerificationString.java
+++ orianna/src/main/java/com/merakianalytics/orianna/types/dto/thirdpartycode/VerificationString.java
@@ -1,9 +1,37 @@
 package com.merakianalytics.orianna.types.dto.thirdpartycode;
 
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.merakianalytics.orianna.types.dto.DataObject;
 
+@JsonDeserialize(using = VerificationString.Deserializer.class)
+@JsonSerialize(using = VerificationString.Serializer.class)
 public class VerificationString extends DataObject {
-    private static final long serialVersionUID = -7327293663423533350L;
+    public static class Deserializer extends JsonDeserializer<VerificationString> {
+        @Override
+        public VerificationString deserialize(final JsonParser parser, final DeserializationContext context) throws IOException {
+            final VerificationString result = new VerificationString();
+            result.string = parser.readValueAs(String.class);
+            return result;
+        }
+    }
+
+    public static class Serializer extends JsonSerializer<VerificationString> {
+        @Override
+        public void serialize(final VerificationString string, final JsonGenerator generator, final SerializerProvider provider) throws IOException {
+            generator.writeObject(string.getString());
+        }
+    }
+
+    private static final long serialVersionUID = -7422547490021592184L;
     private String string, platform;
     private long summonerId;
 
