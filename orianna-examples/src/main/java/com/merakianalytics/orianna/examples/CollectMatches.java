55735624ce74b28783e06f247289c19db20c2c9c
Rob Rua <robertrua@gmail.com>
2018-02-16 22:53:26 -0500

    code formatter

--- orianna-examples/src/main/java/com/merakianalytics/orianna/examples/CollectMatches.java
+++ orianna-examples/src/main/java/com/merakianalytics/orianna/examples/CollectMatches.java
@@ -11,50 +11,49 @@ import com.merakianalytics.orianna.types.core.match.Participant;
 import com.merakianalytics.orianna.types.core.summoner.Summoner;
 
 public class CollectMatches {
-	
-	public static MatchHistory filterMatchHistory(final Summoner summoner) {
-		MatchHistory matchHistory = MatchHistory.forSummoner(summoner).withQueues(Queue.ARAM).withSeasons(Season.SEASON_8).get();
-		return matchHistory;
-	}
-	
+    private static MatchHistory filterMatchHistory(final Summoner summoner) {
+        final MatchHistory matchHistory = MatchHistory.forSummoner(summoner).withQueues(Queue.ARAM).withSeasons(Season.SEASON_8).get();
+        return matchHistory;
+    }
+
     public static void main(final String[] args) {
         final Summoner summoner = Summoner.named("GustavEnk").withRegion(Region.EUROPE_WEST).get();
-        Region region = summoner.getRegion();
+        final Region region = summoner.getRegion();
 
-        HashSet<Long> unpulledSummonerIds = new HashSet<Long>();
+        final HashSet<Long> unpulledSummonerIds = new HashSet<>();
         unpulledSummonerIds.add(summoner.getId());
-        HashSet<Long> pulledSummonerIds = new HashSet<Long>();
-        
-        HashSet<Long> unpulledMatchIds = new HashSet<Long>();
-        HashSet<Long> pulledMatchIds = new HashSet<Long>();
-       
+        final HashSet<Long> pulledSummonerIds = new HashSet<>();
+
+        final HashSet<Long> unpulledMatchIds = new HashSet<>();
+        final HashSet<Long> pulledMatchIds = new HashSet<>();
+
         while(!unpulledSummonerIds.isEmpty()) {
-        	// Get a new summoner from our list of unpulled summoners and pull their match history
-        	long newSummonerId = unpulledSummonerIds.iterator().next();
-        	Summoner newSummoner = Summoner.withId(newSummonerId).withRegion(region).get();
-        	MatchHistory matches = filterMatchHistory(newSummoner);
-        	for(Match match : matches) {
-        		if(!pulledMatchIds.contains(match.getId())) {
-        			unpulledMatchIds.add(match.getId());        			
-        		}
-        	}
-        	unpulledSummonerIds.remove(newSummonerId);
-        	pulledSummonerIds.add(newSummonerId);
-        	
-        	while(!unpulledMatchIds.isEmpty()) {
-        		// Get a random match from our list of matches
-        		long newMatchId = unpulledMatchIds.iterator().next();
-        		Match newMatch = Match.withId(newMatchId).withRegion(region).get();
-        		for(Participant p : newMatch.getParticipants()) {
-        			if(!pulledSummonerIds.contains(p.getSummoner().getId()) && !unpulledSummonerIds.contains(p.getSummoner().getId())) {
-        				unpulledSummonerIds.add(p.getSummoner().getId());
-        			}
-        		}
-        		// The above lines will trigger the match to load its data by iterating over all the participants.
-        		// If you have a database in your datapipeline, the match will automatically be stored in it.
-        		unpulledMatchIds.remove(newMatchId);
-        		pulledMatchIds.add(newMatchId);
-        	}
+            // Get a new summoner from our list of unpulled summoners and pull their match history
+            final long newSummonerId = unpulledSummonerIds.iterator().next();
+            final Summoner newSummoner = Summoner.withId(newSummonerId).withRegion(region).get();
+            final MatchHistory matches = filterMatchHistory(newSummoner);
+            for(final Match match : matches) {
+                if(!pulledMatchIds.contains(match.getId())) {
+                    unpulledMatchIds.add(match.getId());
+                }
+            }
+            unpulledSummonerIds.remove(newSummonerId);
+            pulledSummonerIds.add(newSummonerId);
+
+            while(!unpulledMatchIds.isEmpty()) {
+                // Get a random match from our list of matches
+                final long newMatchId = unpulledMatchIds.iterator().next();
+                final Match newMatch = Match.withId(newMatchId).withRegion(region).get();
+                for(final Participant p : newMatch.getParticipants()) {
+                    if(!pulledSummonerIds.contains(p.getSummoner().getId()) && !unpulledSummonerIds.contains(p.getSummoner().getId())) {
+                        unpulledSummonerIds.add(p.getSummoner().getId());
+                    }
+                }
+                // The above lines will trigger the match to load its data by iterating over all the participants.
+                // If you have a database in your datapipeline, the match will automatically be stored in it.
+                unpulledMatchIds.remove(newMatchId);
+                pulledMatchIds.add(newMatchId);
+            }
         }
     }
 }
