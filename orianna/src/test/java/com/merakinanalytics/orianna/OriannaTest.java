66f2bd6806cc6ea3c5f35a473f345e82a2631019
Levi <thelevimatusproject@gmail.com>
2018-10-12 08:40:00 -0300

    added junit annotions

--- orianna/src/test/java/com/merakinanalytics/orianna/OriannaTest.java
+++ orianna/src/test/java/com/merakinanalytics/orianna/OriannaTest.java
@@ -10,53 +10,57 @@ import com.merakianalytics.orianna.Orianna.Settings;
 import com.merakianalytics.orianna.datapipeline.common.expiration.ExpirationPeriod;
 import com.merakianalytics.orianna.datapipeline.riotapi.RiotAPI;
 import com.merakianalytics.orianna.types.common.Platform;
+
 import org.junit.jupiter.api.*;
+
 
 import java.util.List;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
-class OriannaTest {
 
-    public static class TestConfiguration {
+public class OriannaTest {
+
+    @Nested
+    public static class ConfigurationTest {
 
         private Configuration pipeConfig;
 
         @BeforeEach
-        void setup() {
+        public void setup() {
             this.pipeConfig = new Configuration();
         }
 
         @Test
-        void testDefaultVersionExpiration() {
+        public void testDefaultVersionExpiration() {
             Assertions.assertNotNull(this.pipeConfig.getCurrentVersionExpiration());
             Assertions.assertEquals(this.pipeConfig.getCurrentVersionExpiration().getClass(), ExpirationPeriod.class);
         }
 
         @Test
-        void testDefaultPipelineElements() {
+        public void testDefaultPipelineElements() {
             List<PipelineConfiguration.PipelineElementConfiguration> actualElements = this.pipeConfig.getPipeline().getElements();
             Assertions.assertTrue(actualElements.size() > 0);
         }
 
         @Test
-        void testDefaultPipelineTransformers() {
+        public void testDefaultPipelineTransformers() {
             Set<PipelineConfiguration.TransformerConfiguration> actualTransformers = this.pipeConfig.getPipeline().getTransformers();
             Assertions.assertTrue(actualTransformers.size() > 0);
         }
 
         @Test
-        void testDefaultPlatform() {
+        public void testDefaultPlatform() {
             Assertions.assertNull(this.pipeConfig.getDefaultPlatform());
         }
 
         @Test
-        void testDefaultLocale() {
+        public void testDefaultLocale() {
             Assertions.assertNull(this.pipeConfig.getDefaultLocale());
         }
 
         @Test
-        void testSetCurrentVersionExpiration() {
+        public void testSetCurrentVersionExpiration() {
             ExpirationPeriod newPeriod = ExpirationPeriod.create(24L, TimeUnit.HOURS);
             this.pipeConfig.setCurrentVersionExpiration(newPeriod);
             Assertions.assertEquals(this.pipeConfig.getCurrentVersionExpiration().getPeriod(), 24L);
@@ -64,21 +68,21 @@ class OriannaTest {
         }
 
         @Test
-        void testSetDefaultLocale() {
+        public void testSetDefaultLocale() {
             String locale = "en-US";
             this.pipeConfig.setDefaultLocale(locale);
             Assertions.assertEquals(this.pipeConfig.getDefaultLocale(), locale);
         }
 
         @Test
-        void testSetDefaultPlatform() {
+        public void testSetDefaultPlatform() {
             Platform newPlatform = Platform.NORTH_AMERICA;
             this.pipeConfig.setDefaultPlatform(newPlatform);
             Assertions.assertEquals(this.pipeConfig.getDefaultPlatform(), newPlatform);
         }
 
         @Test
-        void testSetPipeline() {
+        public void testSetPipeline() {
             PipelineConfiguration newPipe = new PipelineConfiguration();
             newPipe.setElements(
                     ImmutableList.of(PipelineConfiguration.PipelineElementConfiguration.defaultConfiguration(RiotAPI.class))
@@ -90,7 +94,7 @@ class OriannaTest {
 
     }
 
-    public static class TestSettings {
+    public static class SettingsTest {
 
         private Settings settings;
 
@@ -112,13 +116,13 @@ class OriannaTest {
         }
 
         @Test
-        void testDefaultPipelineElements() {
+        public void testDefaultPipelineElements() {
             List<PipelineElement> actualElements = this.settings.getPipeline().getElements();
             Assertions.assertTrue(actualElements.size() > 0);
         }
 
         @Test
-        void testDefaultPipelineTransformers() {
+        public void testDefaultPipelineTransformers() {
             Set<DataTransformer> actualTransformers = this.settings.getPipeline().getTransformers();
             Assertions.assertTrue(actualTransformers.size() > 0);
         }
