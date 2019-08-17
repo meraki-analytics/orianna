ecfe111bffc21311af60823013d0e30de8ae5c4b
omarathon <pixzlert@gmail.com>
2019-08-17 21:47:09 +0100

    Add IRON tier to Tier.java, cleanup (#104)

--- orianna/src/main/java/com/merakianalytics/orianna/types/common/Tier.java
+++ orianna/src/main/java/com/merakianalytics/orianna/types/common/Tier.java
@@ -3,15 +3,16 @@ package com.merakianalytics.orianna.types.common;
 import java.util.Comparator;
 
 public enum Tier {
-        BRONZE(1),
-        CHALLENGER(8),
-        DIAMOND(5),
-        GOLD(3),
-        GRANDMASTER(7),
-        MASTER(6),
-        PLATINUM(4),
-        SILVER(2),
-        UNRANKED(0);
+        UNRANKED(0),
+        IRON(1),
+        BRONZE(2),
+        SILVER(3),
+        GOLD(4),
+        PLATINUM(5),
+        DIAMOND(6),
+        MASTER(7),
+        GRANDMASTER(8),
+        CHALLENGER(9);
 
     public static Comparator<Tier> getComparator() {
         return new Comparator<Tier>() {
